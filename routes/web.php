3dd9be1a55a3b3be7bd3d330e62fb0555b332f7d
Mark-Pino <mark.pino@upeu.edu.pe>
2024-10-24 14:32:44 -0500

    Cliente

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,11 @@ Route::get('/empleado/edit/{id}', [EmpleadosController::class, 'edit'])->name('m
 
 Route::put('/empleado/update/{id}', [EmpleadosController::class, 'update'])->name('myUpdate');
 
-
+Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+Route::post('/clientes.store', [ClienteController::class, 'store'])->name('clientes.add');
+Route::delete('/cliente/delete/{id}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
+Route::get('/cliente/edit/{id}', [ClienteController::class, 'edit'])->name('clientes.edit');
+Route::put('/cliente/update/{id}', [ClienteController::class, 'update'])->name('clientes.update');
 
 Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
 Route::post('/categorias.store', [CategoriaController::class, 'store'])->name('categorias.add');
