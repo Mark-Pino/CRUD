64b08b474f2fc5b7e43e8cf627c0a948a732cdc8
Mark-Pino <mark.pino@upeu.edu.pe>
2024-10-24 16:27:45 -0500

    PruebasTDD

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
+    protected $fillable = ['stock'];
     use HasFactory;
+    public function calcularStockTotal()
+    {
+        return $this->stock;
+    }
 }
