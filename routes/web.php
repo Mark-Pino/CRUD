87a054f44126a4bc9be10fa0f431614b65082039
jhanslarico11 <jhanslarico11@gmail.com>
2024-10-24 14:28:27 -0500

    Categoria

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/empleados', [EmpleadosController::class, 'index'])->name('empleados.index');
+
+Route::post('/store', [EmpleadosController::class, 'store'])->name('myStore');
+
+Route::get('/empleado/show/{id}', [EmpleadosController::class, 'show'])->name('myShow');
+
+Route::delete('/empleado/delete/{id}', [EmpleadosController::class, 'destroy'])->name('myDestroy');
+
+Route::get('/empleado/edit/{id}', [EmpleadosController::class, 'edit'])->name('myEdit');
+
+Route::put('/empleado/update/{id}', [EmpleadosController::class, 'update'])->name('myUpdate');
+
+
+
+Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+Route::post('/categorias.store', [CategoriaController::class, 'store'])->name('categorias.add');
+Route::delete('/categoria/delete/{id}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
+Route::get('/categoria/edit/{id}', [CategoriaController::class, 'edit'])->name('categorias.edit');
+Route::put('/categoria/update/{id}', [CategoriaController::class, 'update'])->name('categorias.update');
+
